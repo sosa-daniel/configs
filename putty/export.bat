04584d4844074d83d6fe27d4ea59e3ea2f7eba14
sosa-daniel <152649637+sosa-daniel@users.noreply.github.com>
2024-10-10 16:14:54 -0700

    Delete putty directory

--- putty/export.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-regedit /e default-settings.reg "HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions\Default%%20Settings"
-echo Default settings for PuTTY exported to default-settings.reg
