6116a9481c006fe4c4b0409de5510d2c7fcced77
sosa-daniel <152649637+sosa-daniel@users.noreply.github.com>
2024-10-18 17:53:56 -0700

    Update load_configs.sh

--- load_configs.sh
+++ load_configs.sh
@@ -5,12 +5,8 @@ cd ~
 wget https://raw.githubusercontent.com/sosa-daniel/configs/master/tmux/.tmux.conf
 rm ~/.bashrc
 wget https://raw.githubusercontent.com/sosa-daniel/configs/master/bash/.inputrc
-bind -f  ~/.inputrc
 wget https://raw.githubusercontent.com/sosa-daniel/configs/master/bash/.bashrc
-source ~/.bashrc
 
 install Tmux plug manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
-
-tmux
