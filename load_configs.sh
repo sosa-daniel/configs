256d894ea1012d3107b6a612972377e47ee83800
sosa-daniel <152649637+sosa-daniel@users.noreply.github.com>
2024-10-24 11:03:27 -0700

    Update load_configs.sh

--- load_configs.sh
+++ load_configs.sh
@@ -4,15 +4,13 @@
 cd ~
 mv .tmux.conf .tmux.conf.bak
 mv .inputrc .inputrc.bak
-#mv .bashrc .bashrc.bak
+mv .bashrc .bashrc.bak
 mv .vimrc .vimrc.bak
 wget https://raw.githubusercontent.com/sosa-daniel/configs/master/tmux/.tmux.conf
 wget https://raw.githubusercontent.com/sosa-daniel/configs/master/bash/.inputrc
-#wget https://raw.githubusercontent.com/sosa-daniel/configs/master/bash/.bashrc
+wget https://raw.githubusercontent.com/sosa-daniel/configs/master/bash/.bashrc
 wget https://raw.githubusercontent.com/sosa-daniel/configs/master/vim/.vimrc
 
-echo "PS1='\[\e[1;32m\]┌──[\[\e[m\]\[\e[1;34m\]\D{%b/%d/%Y %-I:%M %p}\[\e[m\]\[\e[1;32m\]]─[\[\e[m\]\u\[\e[1;32m\]@\[\e[m\]\[\e[1;34m\]\h\[\e[m\]\[\e[1;32m\]]─[\[\e[m\]\w\[\e[1;32m\]]\n└──╼ $ \[\e[m\]\[\033[1;32m\]'" >> ~/.bashrc
-echo 'export PS1="$PS1\[\033[1;32m\]"' >> ~/.bashrc
 
 
 install Tmux plug manager
